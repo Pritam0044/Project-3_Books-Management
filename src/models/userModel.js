9e545c85169d8737d9cac7d9907f677e7a0ac4de
PranayRaut-01 <pranayraut17@gmail.com>
2022-05-09 17:17:42 +0530

    added jwt

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -5,25 +5,25 @@ const userSchema = new mongoose.Schema(
     title: {
       type: String,
       required: "title required",
-      enum: ["Mr", "Mrs", "Miss"],
+      enum: ["Mr", "Mrs", "Miss"]
     },
     name: { type: String, required: "name required" },
     phone: { type: String, required: "phone required", unique: true },
     email: {
       type: String,
       required: "email required",
-      unique: true,
+      unique: true
     },
     password: {
       type: String,
       required: "password required",
       minLen: 8,
-      maxLen: 15,
+      maxLen: 15
     },
     address: {
       street: { type: String },
       city: { type: String },
-      pincode: { type: String },
+      pincode: { type: String }
     },
   },
   { timestamp: true }

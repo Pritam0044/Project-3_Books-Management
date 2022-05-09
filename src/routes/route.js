02e6febc61f877b095a54364b4508f28918dc8f1
poonam sandip patel <sarbhadi@gmail.com>
2022-05-09 23:09:12 +0530

    ok

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-
 ///////////////// [ IMPORTED CONTROLLERS ] /////////////////
 const userController= require("../controllers/userController");
 

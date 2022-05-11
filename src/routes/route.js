b02f3a7f0b4ad7c43ae7d16852e4569737d4828c
Pankaj Kumar 98xxxxxx55 <102400319+PankajKumarSingh-Aw@users.noreply.github.com>
2022-05-11 21:30:06 +0530

    minor changes fixed in create review

--- src/routes/route.js
+++ src/routes/route.js
@@ -4,6 +4,7 @@ const router = express.Router();
 ///////////////// [ IMPORTED CONTROLLERS ] /////////////////
 const userController= require("../controllers/userController");
 const bookController = require("../controllers/bookController")
+const reviewController = require("../controllers/reviewController")
 
 
 ///////////////// [ ALL API's HERE ] /////////////////

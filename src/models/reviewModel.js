60b9679c67aed1957d54514e2b48a52980fe7f20
amarjeetdeveloper <102247009+amarjeetdeveloper@users.noreply.github.com>
2022-05-11 09:26:30 +0530

    minor change in

--- src/models/reviewModel.js
+++ src/models/reviewModel.js
@@ -1,28 +1,22 @@
 const mongoose = require("mongoose");
 const ObjectId = mongoose.Schema.Types.ObjectId;
 
-
 const reviewSchema = new mongoose.Schema(
   {
     bookId: {
-        type: ObjectId,
-        required: true,
-        ref: ('Book')
+      type: ObjectId,
+      required: "bookId is required",
+      ref: "Book",
     },
-    reviewedBy: {type:String,
-         required:true,
-         default :'Guest',
+    reviewedBy: {
+      type: String,
+      required: "reviewedBy is required",
+      default: "Guest",
     },
-    reviewedAt: {type:Date,
-                 required:true
-    },
-    rating: {type:Number,
-        required:true,
-         minLength: 1,
-          maxLength: 5
-    },
-    review: {type:String},
-    isDeleted :{type: Boolean, default:false}
+    reviewedAt: { type: Date, required: "reviewedAt is required" },
+    rating: { type: Number, required: "rating is required" },
+    review: { type: String },
+    isDeleted: { type: Boolean, default: false },
   },
   { timestamps: true }
 );

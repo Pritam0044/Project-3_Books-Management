031998acc53bf383c70ab7781ef40b6a68ebea8c
amarjeetdeveloper <102247009+amarjeetdeveloper@users.noreply.github.com>
2022-05-11 09:14:35 +0530

    remove comment in review key

--- src/models/BooksModel.js
+++ src/models/BooksModel.js
@@ -1,20 +1,29 @@
 const mongoose = require("mongoose");
 
-ObjectId = mongoose.Schema.Types.ObjectId
+ObjectId = mongoose.Schema.Types.ObjectId;
 
 const bookSchema = new mongoose.Schema(
-{ 
-    title: {type:String, required:"title is required", unique:true},
-    excerpt: {type:String, required:"excerpt is required"}, 
-    userId: {type:ObjectId, required:"userId is required", ref:"User"},
-    ISBN: {type:String, required:"ISBN is required", unique:true},
-    category: {type:String, required:"category is required"},
-    subcategory: {type:[String], required:"subcategory is required"},
-    reviews: {type:Number, default: 0, comment:" Holds number of reviews of this book"},
-    deletedAt: {Date}, 
-    isDeleted: {type:Boolean, default: false},
-    releasedAt: {type:Date, required:"releasedAt is required"},
+  {
+    title: {
+      type: String,
+      required: "title is required",
+      unique: true,
+      trim: true,
+    },
+    excerpt: { type: String, required: "excerpt is required", trim: true },
+    userId: { type: ObjectId, required: "userId is required", ref: "User" },
+    ISBN: { type: String, required: "ISBN is required", unique: true },
+    category: { type: String, required: "category is required" },
+    subcategory: { type: [String], required: "subcategory is required" },
+    reviews: {
+      type: Number,
+      default: 0,
+    },
+    deletedAt: { Date },
+    isDeleted: { type: Boolean, default: false },
+    releasedAt: { type: Date, required: "releasedAt is required" },
   },
-  {timestamps:true})
+  { timestamps: true }
+);
 
-  module.exports = mongoose.model("Book", bookSchema) //books
+module.exports = mongoose.model("Book", bookSchema); //books
